5c74ecff4294dc675fc80f72ed49a5a59100a023
Nisarga BV <nisargabv19@gmail.com>
2025-04-24 19:37:34 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package com.ee;
+
+
+public class Demo {
+	 public static void main(String args[]) {
+		  employe e1;
+		  e1=new employe();
+		  
+		  employe e2;
+		  e2=new employe();
+		  System.out.println(e1.name);
+		  System.out.println(e1.age);
+		  System.out.println(e1.branch);
+		  
+}
+}
---
+++ employe.java
@@ -0,0 +1,14 @@
+package com.ee;
+
+public class employe {
+	String name="nisarga";
+	String name2="nisarga bv";
+	int age=21;
+	String branch="cse";
+	
+	void work() {
+		System.out.println("employe is working");
+	}
+   
+}
+
