eebcf3cc24c8e4ea149ac915d819d9c2145701c0
Nisarga BV <nisargabv19@gmail.com>
2025-04-24 19:30:27 +0530

    Add files via upload

---
+++ fann.java
@@ -0,0 +1,17 @@
+package com.f;
+
+public class fann {
+	String colour="green";
+	int cost =2500;
+	String brand="usha";
+	
+	void on() {
+		System.out.println("fan is on");
+	}
+	void off() {
+		System.out.println("fan is off");
+	}
+	void rotate() {
+		System.out.println("fan is rotating");
+	}
+	   }
---
+++ revv.java
@@ -0,0 +1,137 @@
+package com.rev;
+
+public class revv {
+	/*public static void main (String[]args){
+		int rows=5;
+		for(int i=1;i<=rows;i++){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=1;j<=i;j++){
+			System.out.print((char)(j+64)+ " ");
+			}
+			for(int j=i-1;j>=1;j--){
+			System.out.print((char)(j+64)+ " ");
+			}
+			System.out.println();
+		}
+		for(int i=rows-1;i>=1;i--){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=1;j<=i;j++){
+			System.out.print((char)(j+64)+ " ");
+			}
+			for(int j=i-1;j>=1;j-- ){
+			System.out.print((char)(j+64)+ " ");
+			}
+			System.out.println();
+		}
+	}*/
+	
+	/*public static void main (String[]args){
+		int rows=5;
+		for(int i=1;i<=rows;i++){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=1;j<=i;j++){
+			System.out.print(j + " ");
+			}
+			for(int j=i-1;j>=1;j--){
+			System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		
+		}
+	}*/
+	
+	/*public static void main (String[]args){
+		int rows=5;
+		for(int i=1;i<=rows;i++){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=(rows+1-i);j<=5;j++){
+			System.out.print(j + " ");
+			}
+			for(int j=4;j>=(rows+1-i);j--){
+			System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		for(int i=rows-1; i>=1; i--){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=(rows+1-i);j<=5;j++){
+			System.out.print(j + " ");
+			}
+			for(int j=4;j>=(rows+1-i);j--){
+			System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+		}
+	}*/
+	
+	/*public static void main (String[]args){
+		int rows=5;
+		for(int i=1;i<=rows;i++){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=(rows+1-i);j<=rows;j++){
+			System.out.print((char)(j+64)+ " ");
+			}
+			for(int j=4;j>=(rows+1-i);j--){
+			System.out.print((char)(j+64)+ " ");
+			}
+			System.out.println();
+		}
+		for(int i=rows;i>=1;i--){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=1;j<=i;j++){
+			System.out.print((char)(j+64)+ " ");
+			}
+			for(int j=i-1;j>=1;j-- ){
+			System.out.print((char)(j+64)+ " ");
+			}
+			System.out.println();
+		}
+	}
+}*/
+	
+	
+	public static void main (String[]args){
+		int rows=5;
+		for(int i=1;i<=rows;i++){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=i;j>=1;j--){
+			System.out.print((char)(j+64)+ " ");
+			}
+			for(int j=2;j<=i;j++){
+			System.out.print((char)(j+64)+ " ");
+			}
+			System.out.println();
+		}
+		for(int i=rows;i>=1;i--){
+			for(int j=1;j<=(rows-i);j++){
+               System.out.print(" "+" "); 
+			}
+			for(int j=1;j<=i;j++){
+			System.out.print((char)(j+64)+ " ");
+			}
+			for(int j=i-1;j>=1;j-- ){
+			System.out.print((char)(j+64)+ " ");
+			}
+			System.out.println();
+		}
+	}
+}
+	     
