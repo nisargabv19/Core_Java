61d5b236b60a824675016663b12b34d5518996dc
Nisarga BV <nisargabv19@gmail.com>
2025-04-24 19:31:28 +0530

    Add files via upload

---
+++ stu/Demo.java
@@ -0,0 +1,13 @@
+package com.stu;
+
+public class Demo {
+  public static void main(String args[]) {
+	  student s1;
+	  s1=new student();
+	  
+	  student s2;
+	  s2=new student();
+	  System.out.println(s1.name);
+	  
+}
+}
---
+++ stu/student.java
@@ -0,0 +1,13 @@
+package com.stu;
+
+public class student {
+	String name="nisarga";
+	String name2="nisarga bv";
+	int age=21;
+	String branch="cse";
+	
+	void study() {
+		System.out.println("student is studying");
+	}
+   
+}
